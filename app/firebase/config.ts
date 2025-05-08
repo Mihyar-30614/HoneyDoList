4b140388d74df0424bf47fcbc14430e5af2dd868
Mihyar Al Masalma <eng.mihyear@gmail.com>
2025-05-08 19:34:52 -0300

    Updating env

--- app/firebase/config.ts
+++ app/firebase/config.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiryIyRULcuI4hspcckFegnkR8FkMrmfk",
-  authDomain: "honeydos-84e3d.firebaseapp.com",
-  projectId: "honeydos-84e3d",
-  storageBucket: "honeydos-84e3d.firebasestorage.app",
-  messagingSenderId: "125150172805",
-  appId: "1:125150172805:web:0779a9302787bc6afa7bec",
-  measurementId: "G-WN0G2GFXT3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
